cd94d17bdd1ae13c63ecce290b2e0688a16763b6
akcaHalit <halitakca070@gmail.com>
2025-07-19 00:35:32 +0300

    We learned how to manage page-based title and navigation settings.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex:1}}>
+
+          <Stack>
+            <Stack.Screen name="index" options={{title: "Anasayfa"}}  />
+            <Stack.Screen name="notifications" 
+            options={{
+              title: "Bildirimler",
+              headerShown: false
+            }}  />
+          </Stack> 
+
+      </SafeAreaView>
+    </SafeAreaProvider>
+
+  );
+
+    {/* First:
+       This way we can edit the settings of the pages. 
+    <Stack.Screen name="index" options={{title: "Anasayfa"}}  />
+    <Stack.Screen name="notifications" 
+    options={{
+      title: "Bildirimler",
+      headerShown: false
+    }}  />
+  </Stack> 
+    */}
+  
+
 }
