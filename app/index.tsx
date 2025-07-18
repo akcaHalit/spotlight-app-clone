4e5c3818547f66869b33f4bdce35ee7f8f60ce05
akcaHalit <halitakca070@gmail.com>
2025-07-19 00:26:53 +0300

    First Navigation Examples. Navigation is supplied with Stack in React Native, that's what we learn here.

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,11 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Link } from "expo-router";
+import { View } from "react-native";
 import { styles } from "../styles/auth.styles";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native</Text>
-      <Image source={ require("../assets/images/icon.png")} 
-      style={{ width: 100, height: 100}}/>
-      
-      
-      <TouchableOpacity onPress={ () => alert("You touched") }>
-        <Text>Press Me!</Text>
-      </TouchableOpacity>
+      <Link href={"/notifications"}>Visit Notification Screen</Link>
     </View>
 
   );
