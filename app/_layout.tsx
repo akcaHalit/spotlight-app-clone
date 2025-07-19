76ac5a1f6f9a19408be1ddd7c69cc4e3b5d4d876
akcaHalit <halitakca070@gmail.com>
2025-07-20 01:14:02 +0300

    * auth.styles.js is updated to auth.styles.ts. * First Login entry was achieved with Clerk on Authentication section in the project.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,23 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+
+
 export default function RootLayout() {
   return (
-    
-    <SafeAreaProvider>
-      <SafeAreaView style={{flex:1, backgroundColor: "#000"}}>
-        
-        <Stack screenOptions={{ headerShown: false}}/>
+    <ClerkProvider tokenCache={tokenCache}>
       
-      </SafeAreaView>
-    </SafeAreaProvider>
+        <SafeAreaProvider>
+          <SafeAreaView style={{flex:1, backgroundColor: "#000"}}>
+            
+            <Stack screenOptions={{ headerShown: false}}/>
+          
+          </SafeAreaView>
+        </SafeAreaProvider>
 
+    </ClerkProvider>
   );
-
-
-  
-
-    {/* First:
-       This way we can edit the settings of the pages. 
-    <Stack.Screen name="index" options={{title: "Anasayfa"}}  />
-    <Stack.Screen name="notifications" 
-    options={{
-      title: "Bildirimler",
-      headerShown: false
-    }}  />
-  </Stack> 
-    */}
-  
 
 }
