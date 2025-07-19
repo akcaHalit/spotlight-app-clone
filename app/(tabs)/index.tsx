7ef285906068e681c3981bfe4b60c05bd24824e1
akcaHalit <halitakca070@gmail.com>
2025-07-19 03:09:32 +0300

    All the process for Setting up the project is done.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { styles } from "../../styles/auth.styles";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Link href={"/notifications"}>Visit Notification Screen</Link>
+      <Link href={"/notifications"}>Feed Screen in tabs</Link>
     </View>
 
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,24 +5,17 @@ export default function RootLayout() {
   return (
     
     <SafeAreaProvider>
-      <SafeAreaView style={{flex:1}}>
-
-        {/* Style= değil screenOptions kullanılır Stack'de. */}
-        {/* Bu */}
-          <Stack screenOptions={{ headerShown: false}}>
-
-            <Stack.Screen name="index" options={{title: "Anasayfa"}}  />
-            <Stack.Screen name="notifications" 
-            options={{
-              title: "Bildirimler",
-              headerShown: false
-            }}  />
-          </Stack> 
-
+      <SafeAreaView style={{flex:1, backgroundColor: "#000"}}>
+        
+        <Stack screenOptions={{ headerShown: false}}/>
+      
       </SafeAreaView>
     </SafeAreaProvider>
 
   );
+
+
+  
 
     {/* First:
        This way we can edit the settings of the pages. 
